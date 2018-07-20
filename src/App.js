efa1f2e6a3ced1ca7260a448cd5695faffa42cf4
Charles Hatch <charlesghatch@gmail.com>
2018-07-20 17:04:03 -0600

    started text and boxshadow effects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import logo from "./logo.svg";
 import trees from "./tree.jpg";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: ""
+    };
+  }
   render() {
+    let { message } = this.state;
+
     return (
       <div>
         <div className="box_model">
@@ -30,10 +38,55 @@ class App extends Component {
         </div>
         <img src={trees} height="6000" width="100%" />
         <div>
-          <h1>Fance</h1>
-          <h1>Fancy</h1>
-          <h1>Fanciest</h1>
-          <div className="backgroundImg">hi</div>
+          <h1 className='' >Fance</h1>
+          <h1 className=''>Fancy</h1>
+          <h1 className=''>Fanciest</h1>
+        </div>
+        <div className="container ">
+          <div className="container grid1">
+            <div className="box">
+              <h1 class="child-title">A</h1>
+            </div>
+            <div className="box">
+              <h1 class="child-title">B</h1>
+            </div>
+            <div className="box">
+              <h1 class="child-title">C</h1>
+            </div>
+            <div className="box">
+              <h1 class="child-title">D</h1>
+            </div>
+            <div className="box">
+              <h1 class="child-title">E</h1>
+            </div>
+            <div className="box">
+              <h1 class="child-title">F</h1>
+            </div>
+          </div>
+          <div className="container grid2">
+            <input
+              type="text"
+              className="test1"
+              placeholder='ellipsis example'
+              onChange={e => this.setState({ message: e.target.value })}
+            />
+            <div className="box">
+              <h1 className="child-title">{message}</h1>
+            </div>
+            <input
+              type="text"
+              className=""
+              onChange={e => this.setState({ message: e.target.value })}
+            />
+            <div className="box">
+              <h1 className="child-title">{message}</h1>
+            </div>
+          </div>
+          <div className="container grid1">
+            <div className="box">
+              <h1 class="child-title">A</h1>
+            </div>
+          </div>
         </div>
       </div>
     );
